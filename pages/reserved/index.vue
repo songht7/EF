c6d985a9ec455a6d243aeec0ebbdf269775edc60
leon ting <leon.ting@okkuaixiu.com>
2019-02-11 15:54:26 +0800

    预约列表

--- pages/reserved/index.vue
+++ pages/reserved/index.vue
@@ -1,7 +1,23 @@
 <template>
-	<view class="page">
-		<view class="content">
-			list
+	<view class="list-block uni-padding-wrap uni-common-mt">
+		<view class="uni-card">
+			<view class="uni-card-content">
+				<view class="uni-card-content-inner">
+					<navigator class="service-head" url="/pages/detail/index">
+						<view class="ser-logo">
+							<image lazy-load src="../../static/ef/customer_qrcode.png" mode="aspectFill"/>
+						</view>
+						<view class="ser-body">
+							<view class="ser-title">英孚</view>
+							<view class="ser-time">预约时间：2019-03-01</view>
+						</view>
+						<view class="ser-tag">免费</view>
+					</navigator>
+					<view class="apply">
+						预约状态：预约成功 等待课程顾问联系
+					</view>
+				</view>
+			</view>
 		</view>
 	</view>
 </template>
@@ -17,5 +33,77 @@
 </script>
 
 <style>
+.uni-card {
+		-webkit-box-shadow: 1px 1px 8px rgba(0, 0, 0, .3);
+		box-shadow: 1px 1px 8px rgba(0, 0, 0, .3);
+		margin: 15px 0;
+	}
 
+	.uni-card-content-inner {
+		padding: 15upx;
+	}
+
+	.service-head {
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-start;
+		align-items: flex-start;
+		position: relative;
+		width: 100%;
+		text-decoration: none;
+		color: #282828;
+	}
+
+	.ser-logo {
+		width: 150upx;
+		height: 150upx;
+	}
+
+	.ser-logo>image {
+		width: 100%;
+		height: 100%;
+	}
+
+	.ser-body {
+		width: 63%;
+		padding-left: 10upx;
+	}
+
+	.ser-title {
+		line-height: 1.4;
+		padding-bottom: 5upx;
+	}
+
+	.ser-describe {
+		line-height: 1.4;
+		color: #525252;
+		font-size: 24upx;
+		margin-bottom: 5upx;
+	}
+
+	.ser-time {
+		line-height: 2;
+		color: #A7A7A7;
+		font-size: 24upx;
+	}
+
+	.ser-tag {
+		font-size: 28upx;
+		text-align: center;
+		padding: 0 15upx;
+		line-height: 2;
+		color: #333;
+		position: absolute;
+		top: -14upx;
+		right: -14upx;
+	}
+
+	.apply {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+		padding: 10upx 0 0;
+		border-top: 1px solid #F5F5F5;
+	}
 </style>
