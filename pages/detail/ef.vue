bcd77f09d82c84dc683d28a326adf2689fba70a3
songht7 <leon.ting@okkuaixiu.com>
2019-02-15 23:44:26 +0800

    center

--- pages/detail/ef.vue
+++ pages/detail/ef.vue
@@ -1,137 +1,137 @@
 <template>
 	<!-- <scroll-view :scroll-top="scrollTop" scroll-y="true" class="scroll-Y" @scrolltoupper="upper" @scrolltolower="lower"
 	 @scroll="scroll"> -->
-		<view class="detail-page-ef">
-			<view class="scroll-view-item detail-block">
-				<view class="ef-top-box">
-					<view class="scroll-view-item ef-top"></view>
-					<view class="uni-padding-wrap uni-common-mt">
-						<view class="uni-card">
-							<view class="uni-card-content">
-								<view class="uni-card-content-inner" id="ApplyFormBox">
-									<view class="ef-title">专业认证培训师<br />教你地道英语</view>
-									<view class="ef-title-sub">FOREIGN TEACHER<br />免费试听体验课</view>
-									<form @submit="formSubmit" @reset="formReset" id="ApplyForm">
-										<view class="form-box">
-											<view class="uni-list">
-												<view class="uni-list-cell">
-													<view class="uni-list-cell-left">
-														姓名
-													</view>
-													<view class="uni-list-cell-db">
-														<input class="uni-input" name="UserName" placeholder="" />
-													</view>
+	<view class="detail-page-ef">
+		<view class="scroll-view-item detail-block">
+			<view class="ef-top-box">
+				<view class="scroll-view-item ef-top"></view>
+				<view class="uni-padding-wrap uni-common-mt">
+					<view class="uni-card">
+						<view class="uni-card-content">
+							<view class="uni-card-content-inner" id="ApplyFormBox">
+								<view class="ef-title">专业认证培训师<br />教你地道英语</view>
+								<view class="ef-title-sub">FOREIGN TEACHER<br />免费试听体验课</view>
+								<form @submit="formSubmit" @reset="formReset" id="ApplyForm">
+									<view class="form-box">
+										<view class="uni-list">
+											<view class="uni-list-cell">
+												<view class="uni-list-cell-left">
+													姓名
+												</view>
+												<view class="uni-list-cell-db">
+													<input class="uni-input" name="UserName" placeholder="" />
 												</view>
 											</view>
-											<view class="uni-list">
-												<view class="uni-list-cell">
-													<view class="uni-list-cell-left">
-														电话
-													</view>
-													<view class="uni-list-cell-db">
-														<input class="uni-input" name="UserPhone" type="number" placeholder="" />
-													</view>
+										</view>
+										<view class="uni-list">
+											<view class="uni-list-cell">
+												<view class="uni-list-cell-left">
+													电话
+												</view>
+												<view class="uni-list-cell-db">
+													<input class="uni-input" name="UserPhone" type="number" placeholder="" />
 												</view>
 											</view>
-											<view class="uni-list half-box">
-												<view class="uni-list-cell">
+										</view>
+										<view class="uni-list half-box">
+											<view class="uni-list-cell">
 
-													<view class="uni-list half">
-														<view class="uni-list-cell">
-															<view class="uni-list-cell-left">
-																年龄
-															</view>
-															<view class="uni-list-cell-db">
-																<picker name="Age" @change="bindChangeAge" :value="age_index" :range="age">
-																	<view class="uni-input">{{age[age_index]}}</view>
-																</picker>
-															</view>
-															<uni-icon size="20" type="arrowdown" color="#DDDDDF"></uni-icon>
+												<view class="uni-list half">
+													<view class="uni-list-cell">
+														<view class="uni-list-cell-left">
+															年龄
 														</view>
-													</view>
-													<view class="uni-list half">
-														<view class="uni-list-cell">
-															<view class="uni-list-cell-left">
-																性别
-															</view>
-															<view class="uni-list-cell-db">
-																<picker name="Gender" @change="bindPickerChange" :value="index" :range="gender">
-																	<view class="uni-input">{{gender[index]}}</view>
-																</picker>
-															</view>
-															<uni-icon size="20" type="arrowdown" color="#DDDDDF"></uni-icon>
+														<view class="uni-list-cell-db">
+															<picker name="Age" @change="bindChangeAge" :value="age_index" :range="age">
+																<view class="uni-input">{{age[age_index]}}</view>
+															</picker>
 														</view>
+														<uni-icon size="20" type="arrowdown" color="#DDDDDF"></uni-icon>
 													</view>
 												</view>
-											</view>
-											<view class="uni-list">
-												<view class="uni-list-cell">
-													<view class="uni-list-cell-left">
-														城市
-													</view>
-													<view class="uni-list-cell-db">
-														<input @click="showMulLinkageTwoPicker" class="uni-input" name="City" disabled :value="pickerText"
-														 placeholder="" />
-													</view>
-												</view>
-											</view>
-											<view class="uni-list apply-date">
-												<view class="uni-list-cell">
-													<view class="uni-list-cell-left">
-														可约时间
-													</view>
-													<view class="uni-list-cell-db">
-														<picker mode="date" name="ApplyDate" :value="date" :start="startDate" :end="endDate" @change="bindDateChange">
-															<view class="uni-input">{{date}}</view>
-														</picker>
+												<view class="uni-list half">
+													<view class="uni-list-cell">
+														<view class="uni-list-cell-left">
+															性别
+														</view>
+														<view class="uni-list-cell-db">
+															<picker name="Gender" @change="bindPickerChange" :value="index" :range="gender">
+																<view class="uni-input">{{gender[index]}}</view>
+															</picker>
+														</view>
+														<uni-icon size="20" type="arrowdown" color="#DDDDDF"></uni-icon>
 													</view>
 												</view>
 											</view>
 										</view>
-										<view class="uni-btn-v">
-											<button formType="submit" :loading="loading" class="apply-btn">立即预约 免费听课</button>
-											<button type="default" class="hide" formType="reset">Reset</button>
+										<view class="uni-list">
+											<view class="uni-list-cell">
+												<view class="uni-list-cell-left">
+													城市
+												</view>
+												<view class="uni-list-cell-db">
+													<input @click="showMulLinkageTwoPicker" class="uni-input" name="City" disabled :value="pickerText"
+													 placeholder="" />
+												</view>
+											</view>
 										</view>
-									</form>
-								</view>
+										<view class="uni-list apply-date">
+											<view class="uni-list-cell">
+												<view class="uni-list-cell-left">
+													可约时间
+												</view>
+												<view class="uni-list-cell-db">
+													<picker mode="date" name="ApplyDate" :value="date" :start="startDate" :end="endDate" @change="bindDateChange">
+														<view class="uni-input">{{date}}</view>
+													</picker>
+												</view>
+											</view>
+										</view>
+									</view>
+									<view class="uni-btn-v">
+										<button formType="submit" :loading="loading" class="apply-btn">立即预约 免费听课</button>
+										<button type="default" class="hide" formType="reset">Reset</button>
+									</view>
+								</form>
 							</view>
 						</view>
 					</view>
 				</view>
-
-				<view class="ef-imgs ef-imgs-left">
-					<image src="../../static/ef/img-1.png" class="ef-img" mode="aspectFit"></image>
-					<view class="img-info img-left-bg">
-						<view class="pro-txt">严格筛选的培训师</view>
-						<view class="pro-desc">专业英语教学认证</view>
-					</view>
-				</view>
-				<view class="ef-imgs ef-imgs-right">
-					<image src="../../static/ef/img-2.png" class="ef-img" mode="aspectFit"></image>
-					<view class="img-info img-right-bg">
-						<view class="pro-txt">培训师全英语交流<br />来自美英加澳等国</view>
-						<view class="pro-desc">培养你与世界对话的能力</view>
-					</view>
-				</view>
-				<view class="ef-imgs ef-imgs-left">
-					<image src="../../static/ef/img-3.png" class="ef-img" mode="aspectFit"></image>
-					<view class="img-info img-left-bg">
-						<view class="pro-txt">中文助教全程协助</view>
-						<view class="pro-desc">耐心陪伴，菜鸟也能飞</view>
-					</view>
-				</view>
-				<view @tap="goTop" v-if="btnShow" class="apply-btn go-top" id="GoTo">立即预约 免费听课</view>
-				<view class="ef-imgs ef-imgs-center">
-					<img src="../../static/ef/img-4.png" class="ef-img" alt="">
-				</view>
-				<img src="../../static/ef/footer-img.jpg" class="ef-img" alt="">
-				<img src="../../static/ef/copyright.png" class="ef-img" alt="">
 			</view>
-			<mpvue-picker :themeColor="themeColor" ref="mpvuePicker" :mode="mode" :deepLength="deepLength" :pickerValueDefault="pickerValueDefault"
-			 @onConfirm="onConfirm" @onCancel="onCancel" :pickerValueArray="pickerValueArray"></mpvue-picker>
-			<mpvue-city-picker :themeColor="themeColor" ref="mpvueCityPicker" :pickerValueDefault="cityPickerValueDefault"
-			 @onCancel="onCancel" @onConfirm="onConfirm"></mpvue-city-picker>
+
+			<view class="ef-imgs ef-imgs-left">
+				<image src="../../static/ef/img-1.png" class="ef-img" mode="aspectFit"></image>
+				<view class="img-info img-left-bg">
+					<view class="pro-txt">严格筛选的培训师</view>
+					<view class="pro-desc">专业英语教学认证</view>
+				</view>
+			</view>
+			<view class="ef-imgs ef-imgs-right">
+				<image src="../../static/ef/img-2.png" class="ef-img" mode="aspectFit"></image>
+				<view class="img-info img-right-bg">
+					<view class="pro-txt">培训师全英语交流<br />来自美英加澳等国</view>
+					<view class="pro-desc">培养你与世界对话的能力</view>
+				</view>
+			</view>
+			<view class="ef-imgs ef-imgs-left">
+				<image src="../../static/ef/img-3.png" class="ef-img" mode="aspectFit"></image>
+				<view class="img-info img-left-bg">
+					<view class="pro-txt">中文助教全程协助</view>
+					<view class="pro-desc">耐心陪伴，菜鸟也能飞</view>
+				</view>
+			</view>
+			<view @tap="goTop" class="apply-btn go-top goTopEF" id="GoTo">立即预约 免费听课</view><!-- v-if="btnShow" -->
+			<view class="ef-imgs ef-imgs-center">
+				<img src="../../static/ef/img-4.png" class="ef-img" alt="">
+			</view>
+			<img src="../../static/ef/footer-img.jpg" class="ef-img" alt="">
+			<img src="../../static/ef/copyright.png" class="ef-img" alt="">
 		</view>
+		<mpvue-picker :themeColor="themeColor" ref="mpvuePicker" :mode="mode" :deepLength="deepLength" :pickerValueDefault="pickerValueDefault"
+		 @onConfirm="onConfirm" @onCancel="onCancel" :pickerValueArray="pickerValueArray"></mpvue-picker>
+		<mpvue-city-picker :themeColor="themeColor" ref="mpvueCityPicker" :pickerValueDefault="cityPickerValueDefault"
+		 @onCancel="onCancel" @onConfirm="onConfirm"></mpvue-city-picker>
+	</view>
 	<!-- </scroll-view> -->
 
 </template>
@@ -140,6 +140,7 @@
 <!-- #endif -->
 <script>
 	import util from '../../common/util.js';
+	const mdl = util.module;
 	import uniIcon from '../../components/uni-icon.vue';
 	//来自 graceUI 的表单验证， 使用说明见手册 http://grace.hcoder.net/doc/info/73-3.html
 	var graceChecker = require("../../common/graceChecker.js");
@@ -201,8 +202,13 @@
 			uni.setNavigationBarTitle({
 				title: "英孚教育 卓著英语培训专家"
 			});
+			var title = "英孚教育 卓著英语培训专家",
+				imgUrl = util.Interface.domain + "/static/ef/billboard.png",
+				dec = "专业认证培训师，教你地道英语";
+			mdl.wxShare("", title, imgUrl, dec);
+			_jquery.setEffectCollect();
 		},
-		onReady:function() {
+		onReady: function() {
 			//_jquery.efftest();
 		},
 		onShow() {
@@ -226,10 +232,10 @@
 			},
 			goTop: function(e) {
 				// 解决view层不同步的问题
-				this.scrollTop = this.old.scrollTop
-				this.$nextTick(function() {
-					this.scrollTop = 0
-				});
+				// 				this.scrollTop = this.old.scrollTop
+				// 				this.$nextTick(function() {
+				// 					this.scrollTop = 0
+				// 				});
 			},
 			bindChangeAge: function(e) {
 				this.age_index = e.target.value
@@ -320,7 +326,7 @@
 								content: "预约失败",
 								showCancel: false
 							})
-						}		
+						}
 						_this.loading = false
 						_this.res = JSON.stringify(result);
 					}
@@ -425,6 +431,7 @@
 	}
 
 	.go-top {
+		display: none;
 		width: 86%;
 		position: fixed;
 		bottom: 0;
