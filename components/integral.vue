5e54d8211ab7d7c625fe8b5a1ff847f2d82b2224
songht7 <leon.ting@okkuaixiu.com>
2019-03-22 22:29:29 +0800

    jf

--- components/integral.vue
+++ components/integral.vue
@@ -1,21 +1,24 @@
 <template name="integral">
 	<view class="pop-box">
-		<view class="pop-head">
-			<view class="pop-title">活动规则</view>
+		<view class="pop-head no-border">
+			<view class="pop-title active-title">积分规则</view>
 			<view class="pop-close" @click="onClick()">
-				<uni-icon type="closeempty" size="42" color="#666666"></uni-icon>
+				<uni-icon type="closeempty" size="42" color="#FEA503"></uni-icon>
 			</view>
+			<view class="cut-line"></view>
 		</view>
 		<view class="pop-main">
 			<view class="intro-box">
 				<view class="intro-block">
 					<view class="intro-h3"></view>
-					<view class="intro-row"><text>1.</text><text> 新用户微信登录并绑定手机号即可获得100积分。</text></view>
-					<view class="intro-row"><text>2.</text><text> 每日转发页面至朋友圈或发送给好友可获得50积分（每日三次）。</text></view>
-					<view class="intro-row"><text>3.</text><text> 报名成功课程还可获得积分奖励</text></view>
-					<view class="intro-row"><text>4.</text><text> 售价￥1的课程，1积分可兑换</text></view>
+					<view class="intro-row"><text class="numb-li">1.</text><text class="txt-orange">每1积分可在申请课程时抵用1元现金</text></view>
+					<view class="intro-row"><text class="numb-li">2.</text><text>新用户微信登录并绑定手机号即可获得30积分。</text></view>
+					<view class="intro-row"><text class="numb-li">3.</text><text>每日转发页面至朋友圈或发送给好友可获得20积分（每日3次）。</text></view>
 				</view>
 			</view>
+		</view>
+		<view class="fix-bottom">
+			<view class="intro-row txt-small txt-center">本活动最终解释权以试听课网为准</view>
 		</view>
 	</view>
 </template>
