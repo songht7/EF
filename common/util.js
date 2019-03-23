c76964df0babb3e92118bbaae6c780d10863f82e
songht7 <leon.ting@okkuaixiu.com>
2019-03-23 20:40:24 +0800

    jf

--- common/util.js
+++ common/util.js
@@ -181,9 +181,9 @@ const module = {
 			wx.updateTimelineShareData(wxSet);
 			// 2. 分享接口
 			// 2.1 监听“分享给朋友”，按钮点击、自定义分享内容及分享结果接口
-			wx.onMenuShareAppMessage(wxSet);
+			//wx.onMenuShareAppMessage(wxSet);
 			// 2.2 监听“分享到朋友圈”按钮点击、自定义分享内容及分享结果接口
-			wx.onMenuShareTimeline(wxSet);
+			//wx.onMenuShareTimeline(wxSet);
 			// 2.3 监听“分享到QQ”按钮点击、自定义分享内容及分享结果接口
 			wx.onMenuShareQQ(wxSet);
 		});
