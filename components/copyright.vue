26ef520540cb1628302d10c224bd1013173eaa3c
Leon Song <leon.ting@okkuaixiu.com>
2019-12-20 17:55:00 +0800

    xxx

--- components/copyright.vue
+++ components/copyright.vue
@@ -4,7 +4,8 @@
 			<view>
 				<a href="//main.meetji.com" class="crlink">梧桫试听课网</a>
 			</view>
-			<a class="crlink" target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=31011702005308">
+			<!-- http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=31011702005308 -->
+			<a class="crlink" target="_blank" href="http://www.beian.miit.gov.cn">
 				<img src="/static/national.png" style="float:left;" />
 				<p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">沪公网安备 31011702005308号</p>
 			</a>
